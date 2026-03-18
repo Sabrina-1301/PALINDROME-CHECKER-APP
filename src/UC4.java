019132d409eac122a1657e5fd4f70d0d28acafe0
Sabrina-1301 <ss2511@srmist.edu.in>
2026-03-18 09:19:12 +0530

    UC4

---
+++ src/UC4.java
@@ -0,0 +1,35 @@
+public class UC4 {
+
+    // Main method - entry point
+    public static void main(String[] args) {
+
+        // Input string
+        String input = "radar";
+
+        // Convert string to character array
+        char[] charArray = input.toCharArray();
+
+        // Two-pointer approach
+        int start = 0;
+        int end = charArray.length - 1;
+
+        boolean isPalindrome = true;
+
+        // Compare characters from both ends
+        while (start < end) {
+            if (charArray[start] != charArray[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+
+        // Display result
+        if (isPalindrome) {
+            System.out.println("The string \"" + input + "\" is a Palindrome.");
+        } else {
+            System.out.println("The string \"" + input + "\" is NOT a Palindrome.");
+        }
+    }
+}
